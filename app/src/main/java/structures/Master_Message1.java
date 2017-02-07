fc1b6acca03a8892de1bb235738749941beabb15
loke09 <lokesh.deshmukh09@gmail.com>
2017-02-08 00:02:22 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/structures/Master_Message1.java
@@ -0,0 +1,71 @@
+package structures;
+
+/**
+ * Created by loke09 on 08/02/17.
+ */
+public class Master_Message1 {
+
+    public int SNO;
+    public String RECIEVER;
+    public String SENDER;
+    public String MESSAGE;
+    public String DATE;
+    public String TIME;
+    public String GENDER;
+
+    public int getSNO() {
+        return SNO;
+    }
+
+    public void setSNO(int SNO) {
+        this.SNO = SNO;
+    }
+
+    public String getRECIEVER() {
+        return RECIEVER;
+    }
+
+    public void setRECIEVER(String RECIEVER) {
+        this.RECIEVER = RECIEVER;
+    }
+
+    public String getSENDER() {
+        return SENDER;
+    }
+
+    public void setSENDER(String SENDER) {
+        this.SENDER = SENDER;
+    }
+
+    public String getMESSAGE() {
+        return MESSAGE;
+    }
+
+    public void setMESSAGE(String MESSAGE) {
+        this.MESSAGE = MESSAGE;
+    }
+
+    public String getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(String DATE) {
+        this.DATE = DATE;
+    }
+
+    public String getTIME() {
+        return TIME;
+    }
+
+    public void setTIME(String TIME) {
+        this.TIME = TIME;
+    }
+
+    public String getGENDER() {
+        return GENDER;
+    }
+
+    public void setGENDER(String GENDER) {
+        this.GENDER = GENDER;
+    }
+}
