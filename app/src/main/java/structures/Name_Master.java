a11f30248dbcfc1f6e35b1eace7df8fe9f2d3311
loke09 <lokesh.deshmukh09@gmail.com>
2017-02-07 23:53:26 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/structures/Name_Master.java
@@ -0,0 +1,53 @@
+package structures;
+
+/**
+ * Created by loke09 on 10/01/17.
+ */
+public class Name_Master {
+
+    public int SNO;
+    public String NAME;
+    public String MOBILE_NUMBER;
+    public String DATE;
+    public String GENDER;
+
+    public int getSNO() {
+        return SNO;
+    }
+
+    public void setSNO(int SNO) {
+        this.SNO = SNO;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getMOBILE_NUMBER() {
+        return MOBILE_NUMBER;
+    }
+
+    public void setMOBILE_NUMBER(String MOBILE_NUMBER) {
+        this.MOBILE_NUMBER = MOBILE_NUMBER;
+    }
+
+    public String getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(String DATE) {
+        this.DATE = DATE;
+    }
+
+    public String getGENDER() {
+        return GENDER;
+    }
+
+    public void setGENDER(String GENDER) {
+        this.GENDER = GENDER;
+    }
+}
